6840e54a8e966b17faf979a7bb7113d9ca538cbf
ImmortalAI <moniki2139@yandex.ru>
2025-04-16 19:45:27 +0300

    insert func into event handler

--- src/App.vue
+++ src/App.vue
@@ -14,15 +14,11 @@ function handleAuth() {
     router.push('/login');
   }
 }
-
-function redirectMainPage() {
-  router.push("/");
-}
 </script>
 
 <template>
   <header class="flex justify-between items-center border-b-neutral-700 border-b-2">
-    <span @click="redirectMainPage()" class="text-2xl m-2 ml-4 cursor-pointer">DEVA</span>
+    <span @click="router.push('/')" class="text-2xl m-2 ml-4 cursor-pointer">DEVA</span>
 
     <div @click="handleAuth()"
       class="border-2 border-violet-900 rounded-2xl p-2 m-2 mr-4 cursor-pointer hover:bg-violet-950 transition-colors">
