098a1f347fcf8c33713797f7295a38fa2c09c831
OblitusNumen <o.aleksashkin@yandex.ru>
2025-04-17 18:21:13 +0300

    refactor LoginView.vue RegisterView.vue
    nice styles

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import { useUserStore } from '@/stores/user';
-import { ref } from 'vue';
-import { useRouter } from 'vue-router';
+import {useUserStore} from '@/stores/user';
+import {ref} from 'vue';
+import {useRouter} from 'vue-router';
 
 const router = useRouter();
 const userStore = useUserStore();
@@ -10,39 +10,61 @@ const password = ref();
 const isFailed = ref(false);
 
 async function login() {
-    // const response = await fetch("/api/login", {
-    //     method: 'POST',
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     },
-    //     body: JSON.stringify({ username: username.value, password: password.value })
-    // });
+  // const response = await fetch("/api/login", {
+  //     method: 'POST',
+  //     headers: {
+  //         'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify({ username: username.value, password: password.value })
+  // });
 
-    // if(response.ok){
-    //     const data = await response.json();
-    //     userStore.username = data?.username;
-    // }
-    // else{
-    //     isFailed.value = true;
-    //     password.value = '';
-    // }
+  // if(response.ok){
+  //     const data = await response.json();
+  //     userStore.username = data?.username;
+  // }
+  // else{
+  //     isFailed.value = true;
+  //     password.value = '';
+  // }
 
-    // For debugging purposes
-    userStore.username = username.value;
-    router.push("/")
+  // For debugging purposes
+  userStore.username = username.value;
+  await router.push("/")
 }
 </script>
 
 <template>
-    <form>
-        <label for="username">Имя пользователя:</label>
-        <input v-model="username" name="username" type="text" placeholder="Username" autocomplete="username" />
-        <label for="password">Пароль:</label>
-        <input autocomplete="current-password" v-model="password" name="password" type="password"
-            placeholder="Password" />
-        <button @click.prevent="login">Login</button>
-        <p v-if="isFailed">Неверный логин или пароль</p>
-        <p>Зарегистрироваться <RouterLink to="/register">здесь</RouterLink>
-        </p>
-    </form>
+  <div class="flex items-center justify-center min-h-screen">
+    <div class="p-6 rounded-xl shadow-md w-full max-w-sm border">
+      <form class="space-y-4 content-center">
+        <div>
+          <label for="password" class="flex justify-center py-2 rounded-lg text-2xl">Вход</label>
+        </div>
+        <!--fixme placeholders-->
+        <div>
+          <label for="username" class="block text-sm font-medium">Имя пользователя:</label>
+          <input v-model="username" name="username" type="text" placeholder="Username" autocomplete="username"
+                 class="w-full mt-1 px-4 py-2 border rounded-lg focus:ring-2" required/>
+        </div>
+        <div>
+          <label for="password" class="block text-sm text-size-sm">Пароль:</label>
+          <input autocomplete="current-password" v-model="password" name="password" type="password"
+                 placeholder="Password" class="w-full mt-1 px-4 py-2 border rounded-lg focus:ring-2" required/>
+        </div>
+        <div class="w-full pt-5">
+          <button @click.prevent="login" type="submit"
+                  class="w-full py-2 rounded-lg border hover:bg-violet-800 hover:translate-y-0.5">
+            Войти
+          </button>
+        </div>
+        <p v-if="isFailed" class="w-full py-2 rounded-lg">Неверный логин или пароль</p>
+        <div class="flex justify-center py-2 rounded-lg">
+          Нет аккаунта?
+          <RouterLink to="/register" class="hover:text-violet-800 pl-2">
+            <u>Зарегистрироваться</u>
+          </RouterLink>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,53 +1,75 @@
 <script setup lang="ts">
-import { useUserStore } from '@/stores/user';
-import { reactive, ref } from 'vue';
-import { RouterLink } from 'vue-router'
+import {useUserStore} from '@/stores/user';
+import {reactive, ref} from 'vue';
+import {RouterLink} from 'vue-router'
 
 const userStore = useUserStore();
 const username = ref();
 const password = ref();
 const repeatPassword = ref();
-const failed = reactive({ isFailed: false, msg: "" });
+const failed = reactive({isFailed: false, msg: ""});
 
 async function register() {
-    if (password.value !== repeatPassword.value) {
-        failed.isFailed = true;
-        failed.msg = "Пароли не совпадают";
-        return;
-    }
+  if (password.value !== repeatPassword.value) {
+    failed.isFailed = true;
+    failed.msg = "Пароли не совпадают";
+    return;
+  }
 
-    const response = await fetch("/api/register", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ username: username.value, password: password.value })
-    });
+  const response = await fetch("/api/register", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({username: username.value, password: password.value})
+  });
 
-    if (response.ok) {
-        const data = await response.json();
-        userStore.username = data?.username;
-    }
-    else {
-        failed.isFailed = true;
-        failed.msg = "Неверный логин или пароль";
-    }
+  if (response.ok) {
+    const data = await response.json();
+    userStore.username = data?.username;
+  } else {
+    failed.isFailed = true;
+    failed.msg = "Неверный логин или пароль";
+  }
 }
 </script>
-
 <template>
-    <form>
-        <label for="username">Имя пользователя:</label>
-        <input v-model="username" name="username" type="text" placeholder="Username" autocomplete="username" />
-        <label for="password">Пароль:</label>
-        <input v-model="password" name="password" type="password" placeholder="Password"
-            autocomplete="current-password" />
-        <label for="repeat-password">Повторите пароль:</label>
-        <input v-model="repeatPassword" name="repeat-password" type="password" placeholder="Repeat Password"
-            autocomplete="current-password" />
-        <button @click.prevent="register">Login</button>
-        <p v-if="failed.isFailed">{{ failed.msg }}</p>
-        <p>Уже есть аккаунт? <RouterLink to="/login">Вход.</RouterLink>
-        </p>
-    </form>
+  <div class="flex items-center justify-center min-h-screen">
+    <div class="p-6 rounded-xl shadow-md w-full max-w-sm border">
+      <form class="space-y-4 content-center">
+        <div>
+          <label for="password" class="flex justify-center py-2 rounded-lg text-2xl">Регистрация</label>
+        </div>
+        <!--fixme placeholders-->
+        <div>
+          <label for="username" class="block text-sm font-medium">Имя пользователя:</label>
+          <input v-model="username" name="username" type="text" placeholder="Username" autocomplete="username"
+                 class="w-full mt-1 px-4 py-2 border rounded-lg focus:ring-2" required/>
+        </div>
+        <div>
+          <label for="password" class="block text-sm text-size-sm">Пароль:</label>
+          <input autocomplete="current-password" v-model="password" name="password" type="password"
+                 placeholder="Password" class="w-full mt-1 px-4 py-2 border rounded-lg focus:ring-2" required/>
+        </div>
+        <div>
+          <label for="repeat-password" class="block text-sm text-size-sm">Пароль:</label>
+          <input autocomplete="current-password" v-model="repeatPassword" name="repeat-password" type="password"
+                 placeholder="Repeat Password" class="w-full mt-1 px-4 py-2 border rounded-lg focus:ring-2" required/>
+        </div>
+        <div class="w-full pt-5">
+          <button @click.prevent="register" type="submit"
+                  class="w-full py-2 rounded-lg border hover:bg-violet-800 hover:translate-y-0.5">
+            Зарегистрироваться
+          </button>
+        </div>
+        <p v-if="failed.isFailed" class="w-full py-2 rounded-lg">{{ failed.msg }}</p>
+        <div class="flex justify-center py-2 rounded-lg">
+          Уже есть аккаунт?
+          <RouterLink to="/login" class="hover:text-violet-800 pl-2">
+            <u>Войти</u>
+          </RouterLink>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
