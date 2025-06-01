3bccebc0af1a5650c5e9e16391a048f7b5c0cc19
ImmortalAI <moniki2139@yandex.ru>
2025-06-01 18:29:54 +0300

    add primevue auto import

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,19 @@ import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 import tailwindcss from '@tailwindcss/vite';
+import Components from 'unplugin-vue-components/vite';
+import {PrimeVueResolver} from '@primevue/auto-import-resolver';
 
 export default defineConfig({
     plugins: [
       vue(),
       vueDevTools(),
       tailwindcss(),
+      Components({
+      resolvers: [
+        PrimeVueResolver()
+      ]
+    }),
     ],
     resolve: {
       alias: {
